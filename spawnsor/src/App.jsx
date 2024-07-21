850b361170cae7f3cc9b2f4955d0e0dce5f83bc6
Rudra <shantanupathak8102@gmail.com>
2024-07-21 10:02:12 +0530

    first commit

---
+++ spawnsor/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+}
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="maintext"> 
+        <h1>Launching Soon...</h1>
+      </div>
+      <div className="textinfo">
+        <p>product under construction</p>
+      </div>
+      <div className="stateinfo">
+          <p className='pointer'>|</p>
+          <p className='currentstatus'> UI/UX designing</p>
+      </div>
+      <div className="status">
+        <div className="statusbar">
+          <div className="visibleprogress"></div>
+        </div>
+        <div className="percentage">
+          <p>0%</p>
+          <p>100%</p>
+        </div>
+      </div>
+      <div className="footer">
+        <p> Copyright &copy; 2024 <span style={{ fontWeight: 'bold' }}>SPAWNSOR</span>. All rights reserved</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
